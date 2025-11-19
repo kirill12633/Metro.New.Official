a1aad79a9bd7d74fae626aab9811b73f38ac2635
Metro.New.Official <Kirilltitovic261111@gmail.com>
2025-11-19 20:54:47 +0200

    Update main.js

--- main/ru/account.settings/assets/js/main.js
+++ main/ru/account.settings/assets/js/main.js
@@ -1,11 +1,11 @@
 // Firebase config (ПОМЕНЯЙ КЛЮЧИ!)
 const firebaseConfig = {
-    apiKey: "ТВОЙ_API_KEY",
-    authDomain: "metro-new-85226.firebaseapp.com",
-    projectId: "metro-new-85226",
-    storageBucket: "metro-new-85226.firebasestorage.app",
-    messagingSenderId: "905640751733",
-    appId: "1:905640751733:web:f1ab3a1b119ca1e245fe3c"
+  apiKey: "AIzaSyDNAyhui3Lc_IX0wuot7_Z6Vdf9Bw5A9mE",
+  authDomain: "metro-new-85226.firebaseapp.com",
+  projectId: "metro-new-85226",
+  storageBucket: "metro-new-85226.firebasestorage.app",
+  messagingSenderId: "905640751733",
+  appId: "1:905640751733:web:f1ab3a1b119ca1e245fe3c"
 };
 
 // Инициализация Firebase
