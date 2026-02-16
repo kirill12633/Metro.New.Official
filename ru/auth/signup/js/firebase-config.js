273aea98df771a4cec208de6f25845128ad3d236
Metro.New.Official <Kirilltitovic261111@gmail.com>
2026-02-16 17:27:40 +0200

    Create firebase-config.js

---
+++ ru/auth/signup/js/firebase-config.js
@@ -0,0 +1,26 @@
+// ============================================
+// FIREBASE КОНФИГУРАЦИЯ
+// ============================================
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDNAyhui3Lc_IX0wuot7_Z6Vdf9Bw5A9mE",
+    authDomain: "metro-new-85226.firebaseapp.com",
+    databaseURL: "https://metro-new-85226-default-rtdb.firebaseio.com",
+    projectId: "metro-new-85226",
+    storageBucket: "metro-new-85226.firebasestorage.app",
+    messagingSenderId: "905640751733",
+    appId: "1:905640751733:web:f1ab3a1b119ca1e245fe3c"
+};
+
+// Инициализация
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+
+// Экспорт
+export { app, auth, db, googleProvider };
