e1ce3c7d73bb21d7b229334a47468e5bf12fd95b
Metro.New.Official <Kirilltitovic261111@gmail.com>
2026-04-17 19:55:07 +0300

    Create firebase-config.js

---
+++ ru/js/firebase-config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDNAyhui3Lc_IX0wuot7_Z6Vdf9Bw5A9mE",
+    authDomain: "metro-new-85226.firebaseapp.com",
+    projectId: "metro-new-85226",
+    storageBucket: "metro-new-85226.firebasestorage.app",
+    messagingSenderId: "905640751733",
+    appId: "1:905640751733:web:f1ab3a1b119ca1e245fe3c"
+};
+
+export const recaptchaConfig = {
+    siteKey: "6LfgAZkrAAAAAOOU9svqDc-yVa23p4BRbEfElYJ-"
+};
